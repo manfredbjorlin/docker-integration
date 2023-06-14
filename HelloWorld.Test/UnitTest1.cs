299d4a711d2e848baa1ebf61937928bfa6ec322e
Manfred Bjørlin <manfred.bjorlin@sysco.no>
2023-06-14 10:01:57 +0200

    testing

--- HelloWorld.Test/UnitTest1.cs
+++ HelloWorld.Test/UnitTest1.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using HelloWorld;
+
 namespace HelloWorld.Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async void Test1()
     {
-        
+        await using var application = new WebApplicationFactory<Program>();
     }
 }
