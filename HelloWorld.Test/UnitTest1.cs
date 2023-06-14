d0cda9d311ce7b8a152658f95926e46257b40a1f
Manfred Bjørlin <manfred.bjorlin@sysco.no>
2023-06-14 10:07:00 +0200

    First test

--- HelloWorld.Test/UnitTest1.cs
+++ HelloWorld.Test/UnitTest1.cs
@@ -6,8 +6,13 @@ namespace HelloWorld.Test;
 public class UnitTest1
 {
     [Fact]
-    public async void Test1()
+    public async void TestDefault()
     {
         await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        var response = await client.GetStringAsync("/");
+
+        Assert.Equal("You shouldn't be here!", response);
     }
 }
--- HelloWorld/README.md
+++ HelloWorld/README.md
@@ -3,3 +3,5 @@
 1. Define models in Services/NGModels.cs
 2. Define method for handling messages from ServiceBus queue in ServiceBus/ServiceBusHandler.cs
 3. Define Http endpoints in Program.cs
+
+How to set up project and test: https://www.twilio.com/blog/test-aspnetcore-minimal-apis
